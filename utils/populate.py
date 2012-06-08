7fca8bad375576d147c7b08519b3ebe68974a642
giulivo <giulivo@fedoraproject.org>
2012-06-08 20:19:55 +0200

    needs to activate virtenv

--- utils/populate.py
+++ utils/populate.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 #
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(here, '..', 'misc/virtenv/lib/python2.6/site-packages')
+virtualenv = os.path.join(here, '..', 'misc/virtenv/bin/activate_this.py')
+execfile(virtualenv, dict(__file__=virtualenv))
+
 # feed parser
 from feedparser import parse
 
